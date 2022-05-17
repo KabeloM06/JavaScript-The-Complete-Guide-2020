bf876771d2438df2f89ee3d0200a46303be58fb0
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-05-17 10:01:11 +0200

    Basics of creating arrays

---
+++ scripts.js
@@ -0,0 +1,16 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+const moreNumbers = new Array(4, 5, 6);
+console.log(moreNumbers);
+
+// This method has a weird feature when passing in just one items as a number
+// The single number will then be used to create an empty array with the number being the length of the array
+const weirdNumbers = new Array(6);
+console.log(weirdNumbers);
+
+const noNewNumbers = Array(7, 8, 9); // This also has the weird length issues when passing just a number
+console.log(noNewNumbers);
+
+const yetMoreNumbers = Array.of(1, 2);
+
